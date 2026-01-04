360db53627c60b6fa819ce47f41f0dab0c76d507
Keroosha <mr.dead.toast@gmail.com>
2026-01-05 00:05:33 +0300

    Bump high ports

--- block-p2p.sh
+++ block-p2p.sh
@@ -49,7 +49,7 @@ add_default_interface "$default_int"
 LOG_PREFIX="TORRENT_BLOCK"
 MAX_ENTRIES=100000
 BLOCK_DURATION=600  # Duration in seconds (10 min)
-HIGH_PORTS="6881:65535"
+HIGH_PORTS="1024:65535"
 
 # Paths for the log file (adjust according to your system)
 LOG_FILE="/var/log/kern.log"
